899f9e4ba73456eb8e89262b2db1c9bc657729a3
ThauaneSP <thataspg@gmail.com>
2017-03-14 11:22:16 -0300

    Correcoes no coelho

--- Assets/Scripts/PranchaMove.cs
+++ Assets/Scripts/PranchaMove.cs
@@ -6,8 +6,10 @@ public class PranchaMove : MonoBehaviour {
     public float velocidade;
     public float limite;
     public float retorno;
+    public float limite;
+    public float retorno;
 
-	void Start () {
+    void Start () {
 		
 	}
 	
