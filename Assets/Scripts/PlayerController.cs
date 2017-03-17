f2512adb9ea464d2497034a88892716064304619
Aluno <aluno@udicentro.local>
2017-03-17 16:57:39 -0300

    Começo da Animação do Player

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private Animator anim;
+    private Rigidbody rb;
+    private bool pulando = false;
+    public float ForcaDoPulo = 10f;
+
+	void Start () {
+
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();	
+	}
+	
+	void Update () {
+
+        if (Input.GetMouseButtonDown(0)) {
+            anim.Play("Pulando");
+            rb.useGravity = true;
+            pulando = true;
+
+        }
+	}
+
+    void FixedUpdate() {
+        if (pulando) {
+            pulando = false;
+            rb.velocity = Vector3.zero;
+            rb.AddForce(Vector3.up * ForcaDoPulo, ForceMode.Impulse);
+        }
+
+    }
+
+
+}
