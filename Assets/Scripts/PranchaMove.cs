de0a297f5a9d6fb0ad0e3650822a95e6f37bf674
ThauaneSP <thataspg@gmail.com>
2017-03-21 10:25:00 -0300

    Colisor da Prancha e do Teto

--- Assets/Scripts/PranchaMove.cs
+++ Assets/Scripts/PranchaMove.cs
@@ -3,15 +3,20 @@ using UnityEngine;
 
 public class PranchaMove : MonoBehaviour {
 
+    private Animator anim;
+    private Rigidbody rb;
+    private AudioSource audiosource;
+    public AudioClip somMorte;
+
     public float velocidade;
     public float limite;
     public float retorno;
 
-    void Start () {
-		
-	}
-	
-	void Update () {
+    void Start() {
+
+    }
+
+    void Update() {
         Vector3 velocidadevetorial = Vector3.left * velocidade;
 
         transform.localPosition = transform.localPosition + velocidadevetorial * Time.deltaTime;
@@ -19,6 +24,16 @@ public class PranchaMove : MonoBehaviour {
         if (transform.localPosition.x <= limite) {
             transform.localPosition = new Vector3(retorno, transform.localPosition.y, transform.localPosition.z);
         }
-            
-	}
+    }
+    void OnCollisionEnter(Collision outro) {
+        if (GameController.instancia.estado == Estado.Jogando) {
+            if (outro.gameObject.tag == "Obstaculo") {
+                rb.AddForce(new Vector3(-50f, 20f, 0), ForceMode.Impulse);
+                rb.detectCollisions = false;
+                anim.Play("Morrendo");
+                audiosource.PlayOneShot(somMorte);
+                GameController.instancia.PlayerMorreu();
+            }
+        }
+    }
 }
